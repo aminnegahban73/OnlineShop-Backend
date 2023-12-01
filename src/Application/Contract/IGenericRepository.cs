87659cb7fd1c6182a28d62b9e3c4e4caa2fdb109
aminnegahban73 <amin.negahban1@gmail.com>
2023-12-01 18:46:32 +0330

    Creating IGenericRepository contract.

---
+++ src/Application/Contract/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contract
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyCollection<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(int id);
+    }
+}

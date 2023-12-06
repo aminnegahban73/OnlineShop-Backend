7150e72356be03caf408a1678a47e0b57891cdf6
Amin Negahban <amin.negahban1@gmail.com>
2023-12-06 23:56:00 +0330

    Adding Dtos and AutoMapper.

---
+++ src/Application/Dtos/Products/ProductDto.cs
@@ -0,0 +1,22 @@
+﻿using Application.Dtos.Common;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.Products
+{
+    public class ProductDto : CommandDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int ProductTypeId { get; set; }
+        public int ProductBrandId { get; set; }
+        public string ProductType { get; set; }   // Title
+        public string ProductBrand { get; set; }  // Title
+    }
+}
